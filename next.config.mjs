2b73cfe093f614b591fb80e556a68011a62950da
chatgph2024 <admin@chatgph.cloud>
2025-03-14 14:52:14 +0000

    Commit changes

--- next.config.mjs
+++ next.config.mjs
@@ -21,6 +21,16 @@ const nextConfig = {
     parallelServerBuildTraces: true,
     parallelServerCompiles: true,
   },
+  env: {
+    NEXT_PUBLIC_FIREBASE_API_KEY: "AIzaSyDtNedkJo6ikNneZZdrheiWbE3Dn2B8kwQ",
+    NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: "ces-project-f8b4e.firebaseapp.com",
+    NEXT_PUBLIC_FIREBASE_DATABASE_URL: "https://ces-project-f8b4e-default-rtdb.asia-southeast1.firebasedatabase.app",
+    NEXT_PUBLIC_FIREBASE_PROJECT_ID: "ces-project-f8b4e",
+    NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET: "ces-project-f8b4e.firebasestorage.app",
+    NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: "580767851656",
+    NEXT_PUBLIC_FIREBASE_APP_ID: "1:580767851656:web:bdf8ae673ba6fb4acdeb3d",
+    NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID: "G-HBRZPQPWN7"
+  }
 }
 
 mergeConfig(nextConfig, userConfig)
